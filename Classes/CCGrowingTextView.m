26625ea2e7448b3ef5931f400a7f68835682f44d
Ivan Ziryanov <ivan.ziryanov@gmail.com>
2013-10-30 17:08:15 +0400

    setting initial frame

--- Classes/CCGrowingTextView.m
+++ Classes/CCGrowingTextView.m
@@ -38,13 +38,27 @@
 {
     __weak UITextView *wself = self;
     _CCGrowingTextViewTextChangedNotification = [[NSNotificationCenter defaultCenter] addObserverForName:UITextViewTextDidChangeNotification object:self queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
-        double delayInSeconds = .001;
+        double delayInSeconds = .05;
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
             if (wself.contentSize.height != wself.frame.size.height)
                 [wself invalidateIntrinsicContentSize];
         });
     }];
+}
+
+- (void)didMoveToSuperview
+{
+    [super didMoveToSuperview];
+    if (self.text.length)
+        return;
+    double delayInSeconds = .05;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        super.text = @"";
+        [self invalidateIntrinsicContentSize];
+        super.text = @"";
+    });
 }
 
 - (void)dealloc
