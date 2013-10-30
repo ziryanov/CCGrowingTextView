c278ab63bca6d67540817852dfe632ad26153abb
Ivan Ziryanov <ivan.ziryanov@gmail.com>
2013-10-30 17:22:44 +0400

    removing extra code

--- Classes/CCGrowingTextView.m
+++ Classes/CCGrowingTextView.m
@@ -55,9 +55,7 @@
     double delayInSeconds = .05;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-        super.text = @"";
         [self invalidateIntrinsicContentSize];
-        super.text = @"";
     });
 }
 
