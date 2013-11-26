3cf7368c88f31131cb3c92d25b4e2a8be0d4e658
Ivan Ziryanov <ivan.ziryanov@gmail.com>
2013-11-26 21:26:49 +0400

    bug with no invalidating size after (setText:) fixed

--- Classes/CCGrowingTextView.m
+++ Classes/CCGrowingTextView.m
@@ -72,7 +72,12 @@
 - (void)setText:(NSString *)text
 {
     super.text = text;
-    [self invalidateIntrinsicContentSize];
+    super.text = text;
+    double delayInSeconds = .05;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self invalidateIntrinsicContentSize];
+    });
 }
 
 @end
